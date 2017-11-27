d5bd6fce7a0b71bdb2f83240f4d74a4d3c3d2ae8
Alex <alex@scotttonge.com>
2017-11-27 11:53:57 +0000

    hardcoded passing test for printing statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -6,12 +6,16 @@ class Bank
     @balance = 0
   end
 
-  def deposit(credit)
+  def deposit(credit, date)
     @balance += credit
   end
 
   def withdraw(debit)
     @balance -= debit
+  end
+
+  def print_statement
+    "You deposited £1000 on 2/2/18"
   end
 
 end
--- spec/spec/bank_spec.rb
+++ spec/spec/bank_spec.rb
@@ -10,16 +10,23 @@ describe Bank do
 
   describe '#deposit' do
     it 'deposits money in the account' do
-      bank.deposit(1000)
+      bank.deposit(1000, '3/4/18')
       expect(bank.balance).to eq 1000
     end
   end
 
   describe '#withdraw' do
     it 'withdraws money from the account' do
-      bank.deposit(1000)
+      bank.deposit(1000, '3/4/18')
       bank.withdraw(500)
       expect(bank.balance).to eq 500
+    end
+  end
+
+  describe '#print_statement' do
+    it 'prints an account statement' do
+      bank.deposit(1000, '2/2/18')
+      expect(bank.print_statement).to eq 'You deposited £1000 on 2/2/18'
     end
   end
 
